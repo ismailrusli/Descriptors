54332993fb68f60b1322def54b2b4271555c5eff
ismail-rusli <ismail_indonesia@yahoo.com>
2015-07-02 15:01:21 +0700

    display video from webcam

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,22 @@
 #include<iostream>
+#include<opencv2/opencv.hpp>
 
-using namespace std
+using namespace std;
 
 int main(int argc, char** argv)
 {
+    cv::VideoCapture cap(0);
+    if(!cap.isOpened())
+        return -1;
 
+    cv::namedWindow("video",1);
+    for(;;)
+    {
+        cv::Mat frame;
+        cap >> frame;
+        cv::imshow("video",frame);
+        if(cv::waitKey(30) >= 0) break;
+    }
 
-  return 0;
+    return 0;
 }
